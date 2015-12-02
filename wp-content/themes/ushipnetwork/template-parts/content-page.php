ed1fbe48f8a7f12196d4928af0e7618fd2d0c4b2
unknown <rmikeska@rudym.local.uship.com>
2015-12-01 19:04:46 -0600

    more modules

--- wp-content/themes/ushipnetwork/template-parts/content-page.php
+++ wp-content/themes/ushipnetwork/template-parts/content-page.php
@@ -17,21 +17,56 @@
 				<?php $hero_small_image = get_sub_field('hero_small_image'); ?>
 
 				<section class="heroSmall" style="background-image: url('<?php echo $hero_small_image['url']; ?>');">
-					<h1 class="heroSmall-text"><?php the_sub_field('hero_small_text'); ?></h1>
+					<div class="heroSmall-container">
+						<h1 class="heroSmall-text"><?php the_sub_field('hero_small_text'); ?></h1>
+					</div>
 				</section>
 
 			<?php elseif (get_row_layout() == 'long_form_content_with_divider'): ?>
 
-				<section class="longFormContent-withDivider">
-					<?php while( have_rows('long_form_content_with_divider_content') ): the_row();
+				<section class="longFormContentWithDivider">
+					<div class="longFormContentWithDivider-container">
+						<?php while( have_rows('long_form_content_with_divider_content') ): the_row(); ?>
 
-						$text_block = get_sub_field('long_form_content_with_divider_content_text_block');
+						<p class="longFormContentWithDivider-text"><?php the_sub_field('long_form_content_with_divider_content_text_block'); ?></p>
 
-					?>
+						<?php endwhile; ?>
+					</div>
+				</section>
 
-					<p><?php the_sub_field('long_form_content_with_divider_content_text_block'); ?></p>
+			<?php elseif (get_row_layout() == 'strip_copy_callout'): ?>
 
-					<?php endwhile; ?>
+				<section class="stripCopyCallout">
+					<div class="stripCopyCallout-container">
+						<h3 class="stripCopyCallout-text"><?php the_sub_field('strip_copy_callout_text'); ?></h3>
+					</div>
+				</section>
+
+			<?php elseif (get_row_layout() == 'full_width_image'): ?>
+
+				<?php $full_width_image_image = get_sub_field('full_width_image_image'); ?>
+
+				<section class="fullWidthImage">
+					<img class="fullWidthImage-image" src="<?php echo $full_width_image_image['url']; ?>">
+				</section>
+
+			<?php elseif (get_row_layout() == 'strip_data_callout'): ?>
+
+				<section class="stripDataCallout">
+					<div class="stripDataCallout-container">
+						<?php while( have_rows('strip_data_callout_block') ): the_row(); ?>
+							<?php while( have_rows('strip_data_callout_block_content') ): the_row(); ?>
+
+								<div class="stripDataCallout-block">
+									<div class="stripDataCallout-block-inner">
+										<span class="stripDataCallout-block-number"><?php the_sub_field('strip_data_callout_block_content_number'); ?></span>
+										<span class="stripDataCallout-block-title"><?php the_sub_field('strip_data_callout_block_content_title'); ?></span>
+									</div>
+								</div>
+
+							<?php endwhile; ?>
+						<?php endwhile; ?>
+					</div>
 				</section>
 
 			<?php endif; ?>
