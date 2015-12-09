0680ceb91c4c040c8bf62591b9784072162ccbec
unknown <rmikeska@rudym.local.uship.com>
2015-12-09 13:24:04 -0600

    adding testimonials and bios modules

--- wp-content/themes/ushipnetwork/template-parts/content-page.php
+++ wp-content/themes/ushipnetwork/template-parts/content-page.php
@@ -26,7 +26,7 @@
 
 				<section class="longFormContentWithDivider">
 					<div class="longFormContentWithDivider-container">
-						<?php while( have_rows('long_form_content_with_divider_content') ): the_row(); ?>
+						<?php while(have_rows('long_form_content_with_divider_content')): the_row(); ?>
 
 						<p class="longFormContentWithDivider-text"><?php the_sub_field('long_form_content_with_divider_content_text_block'); ?></p>
 
@@ -36,7 +36,9 @@
 
 			<?php elseif (get_row_layout() == 'strip_copy_callout'): ?>
 
-				<section class="stripCopyCallout">
+				<?php $section_bg_color = get_sub_field('strip_copy_callout_background_color'); ?>
+
+				<section class="stripCopyCallout <?php echo $section_bg_color; ?>">
 					<div class="stripCopyCallout-container">
 						<h3 class="stripCopyCallout-text"><?php the_sub_field('strip_copy_callout_text'); ?></h3>
 					</div>
@@ -46,22 +48,73 @@
 
 				<?php $full_width_image_image = get_sub_field('full_width_image_image'); ?>
 
-				<section class="fullWidthImage">
-					<img class="fullWidthImage-image" src="<?php echo $full_width_image_image['url']; ?>">
+				<section class="fullWidthImage" style="background-image: url('<?php echo $full_width_image_image['url']; ?>');">
 				</section>
 
 			<?php elseif (get_row_layout() == 'strip_data_callout'): ?>
 
 				<section class="stripDataCallout">
 					<div class="stripDataCallout-container">
-						<?php while( have_rows('strip_data_callout_block') ): the_row(); ?>
-							<?php while( have_rows('strip_data_callout_block_content') ): the_row(); ?>
+						<?php while(have_rows('strip_data_callout_block')): the_row(); ?>
+							<?php while(have_rows('strip_data_callout_block_content')): the_row(); ?>
 
 								<div class="stripDataCallout-block">
 									<div class="stripDataCallout-block-inner">
 										<span class="stripDataCallout-block-number"><?php the_sub_field('strip_data_callout_block_content_number'); ?></span>
 										<span class="stripDataCallout-block-title"><?php the_sub_field('strip_data_callout_block_content_title'); ?></span>
 									</div>
+								</div>
+
+							<?php endwhile; ?>
+						<?php endwhile; ?>
+					</div>
+				</section>
+
+			<?php elseif (get_row_layout() == 'testimonials'): ?>
+
+				<?php $section_bg_color = get_sub_field('testimonials_background_color'); ?>
+
+				<section class="testimonials <?php echo $section_bg_color; ?>">
+					<div class="testimonials-container">
+						<h3 class="testimonials-intro"><?php the_sub_field('testimonials_intro'); ?></h3>
+
+						<?php while(have_rows('testimonials_block')): the_row(); ?>
+							<?php while(have_rows('testimonials_block_content')): the_row(); ?>
+
+								<?php $testimonials_image = get_sub_field('testimonials_image'); ?>
+
+								<div class="testimonials-block">
+									<div class="testimonials-image" style="background-image: url('<?php echo $testimonials_image['url']; ?>');"></div>
+									<p class="testimonials-text"><?php the_sub_field('testimonials_text'); ?></p>
+									<p class="testimonials-source"><?php the_sub_field('testimonials_source'); ?></p>
+									<p class="testimonials-category"><?php the_sub_field('testimonials_category'); ?></p>
+								</div>
+
+							<?php endwhile; ?>
+						<?php endwhile; ?>
+					</div>
+				</section>
+
+			<?php elseif (get_row_layout() == 'bios'): ?>
+
+				<?php $section_bg_color = get_sub_field('bios_background_color'); ?>
+
+				<section class="bios <?php echo $section_bg_color; ?>">
+					<div class="bios-container">
+						<h3 class="bios-intro"><?php the_sub_field('bios_intro'); ?></h3>
+						<h6 class="bios-subintro"><?php the_sub_field('bios_intro_sub'); ?></h6>
+
+						<?php while(have_rows('bios_block')): the_row(); ?>
+							<?php while(have_rows('bios_block_content')): the_row(); ?>
+
+								<?php $bios_image = get_sub_field('bios_image'); ?>
+								<?php $bios_linkedin_url = get_sub_field('bios_linkedin'); ?>
+
+								<div class="bios-block">
+									<div class="bios-image" style="background-image: url('<?php echo $bios_image['url']; ?>');"></div>
+									<p class="bios-name"><?php the_sub_field('bios_name'); ?></p>
+									<p class="bios-description"><?php the_sub_field('bios_description'); ?></p>
+									<a class="bios-linkedin buttonGreenTransparent" target="_blank" href="<?php echo $bios_linkedin_url; ?>">LinkedIn</a>
 								</div>
 
 							<?php endwhile; ?>
