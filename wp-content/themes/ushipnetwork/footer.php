88cf3350c4ccb1772a5ddbe372e0c37e38956d02
unknown <rmikeska@rudym.local.uship.com>
2015-12-10 14:46:40 -0600

    adding bookend module and site footer

--- wp-content/themes/ushipnetwork/footer.php
+++ wp-content/themes/ushipnetwork/footer.php
@@ -14,6 +14,16 @@
   </main>
 
   <footer class="pageFooter">
+    <div class="pageFooter-container">
+      <small class="pageFooter-legal">Copyright &copy; 2015, uShip Inc. and its licensors. All rights reserved.</small>
+      <div class="pageFooter-links">
+        <ul class="pageFooter-links-list">
+          <li class="pageFooter-links-list-item"><a href="#" target="_blank">uShip User Agreement</a></li>
+          <li class="pageFooter-links-list-item"><a href="#" target="_blank">Privacy Policy</a></li>
+          <li class="pageFooter-links-list-item"><a href="#" target="_blank">uShip Site Map</a></li>
+        </ul>
+      </div>
+    </div>
   </footer>
 </div>
 
--- wp-content/themes/ushipnetwork/template-parts/content-page.php
+++ wp-content/themes/ushipnetwork/template-parts/content-page.php
@@ -122,6 +122,21 @@
 					</div>
 				</section>
 
+			<?php elseif (get_row_layout() == 'bookend'): ?>
+
+				<?php $bookend_cta_url = get_sub_field('bookend_cta_url'); ?>
+				<?php $bookend_previous_url = get_sub_field('bookend_previous_url'); ?>
+				<?php $bookend_next_url = get_sub_field('bookend_next_url'); ?>
+
+				<section class="bookend">
+					<div class="bookend-container">
+						<h3 class="bookend-intro"><?php the_sub_field('bookend_intro'); ?></h3>
+						<a class="bookend-cta buttonGreen" href="<?php echo $bookend_cta_url; ?>"><?php the_sub_field('bookend_cta_text'); ?></a>
+						<a class="bookend-nav bookend-navPrevious" href="<?php echo $bookend_previous_url; ?>"><?php the_sub_field('bookend_previous_text'); ?></a>
+						<a class="bookend-nav bookend-navNext" href="<?php echo $bookend_next_url; ?>"><?php the_sub_field('bookend_next_text'); ?></a>
+					</div>
+				</section>
+
 			<?php endif; ?>
 		<?php endwhile; ?>
 	<?php endif; ?>
