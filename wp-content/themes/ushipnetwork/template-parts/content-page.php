8b47d196238714fd6b5341dbb9ff5ec95223750e
unknown <rmikeska@rudym.local.uship.com>
2015-12-30 15:31:06 -0600

    add press page

--- wp-content/themes/ushipnetwork/template-parts/content-page.php
+++ wp-content/themes/ushipnetwork/template-parts/content-page.php
@@ -189,6 +189,32 @@
 					</div>
 				</section>
 
+			<?php elseif (get_row_layout() == 'contact'): ?>
+
+				<?php $section_bg_color = get_sub_field('contact_background_color'); ?>
+
+				<section class="contact <?php echo $section_bg_color; ?>">
+					<div class="contact-container">
+						<div class="contact-info">
+							<h3 class="contact-info-intro"><?php the_sub_field('contact_intro'); ?></h3>
+							<div class="contact-info-text"><?php the_sub_field('contact_text'); ?></div>
+						</div>
+						<div class="contact-links">
+							<div class="contact-links-content">
+								<?php while(have_rows('contact_link')): the_row(); ?>
+									<?php while(have_rows('contact_link_content')): the_row(); ?>
+
+										<?php $contact_link_url = get_sub_field('contact_link_url'); ?>
+
+										<a class="emphasisLink" target="_blank" href="<?php echo $contact_link_url; ?>"><?php the_sub_field('contact_link_text'); ?></a>
+
+									<?php endwhile; ?>
+								<?php endwhile; ?>
+							</div>
+						</div>
+					</div>
+				</section>
+
 			<?php elseif (get_row_layout() == 'bookend'): ?>
 
 				<?php $bookend_cta_url = get_sub_field('bookend_cta_url'); ?>
