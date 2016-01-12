ee4b4fe4e4be8be22de801e569e9062a536b2850
unknown <rmikeska@rudym.local.uship.com>
2016-01-12 17:39:14 -0600

    remove retina.js

--- wp-content/themes/ushipnetwork/functions.php
+++ wp-content/themes/ushipnetwork/functions.php
@@ -120,8 +120,6 @@ function ushipnetwork_scripts() {
 
 	wp_enqueue_script( 'ushipnetwork-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-	wp_enqueue_script( 'ushipnetwork-retina-images', get_template_directory_uri() . '/js/retina.min.js', array(), '20160112', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
