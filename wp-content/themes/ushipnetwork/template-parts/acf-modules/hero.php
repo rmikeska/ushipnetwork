16b6c36a9fe2042ef35548356515be33fb1c3dea
unknown <rmikeska@rudym.local.uship.com>
2016-01-13 13:45:36 -0600

    add new logos and make header and hero responsive

--- wp-content/themes/ushipnetwork/template-parts/acf-modules/hero.php
+++ wp-content/themes/ushipnetwork/template-parts/acf-modules/hero.php
@@ -4,7 +4,9 @@
 <section class="hero <?php echo $hero_size; ?>" style="background-image: url('<?php echo $hero_image['url']; ?>');">
   <div class="hero-container">
     <h1 class="hero-headline"><?php the_sub_field('hero_headline'); ?></h1>
-    <h5 class="hero-subheadline"><?php the_sub_field('hero_subheadline'); ?></h5>
+    <?php while(have_rows('hero_subheadline')): the_row(); ?>
+      <h5 class="hero-subheadline"><?php the_sub_field('hero_subheadline'); ?></h5>
+    <?php endwhile; ?>
     <?php while(have_rows('hero_cta')): the_row(); ?>
       <?php $hero_cta_url = get_sub_field('hero_cta_url'); ?>
       <a class="hero-cta buttonGreen buttonLarge" href="<?php echo $hero_cta_url; ?>"><?php the_sub_field('hero_cta_text'); ?></a>
