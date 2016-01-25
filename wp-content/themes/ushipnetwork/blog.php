ffc3d33f456eb4cc5807025747aa71047391a46c
unknown <rmikeska@rudym.local.uship.com>
2016-01-25 15:12:41 -0600

    add some blog home styles and structure

--- wp-content/themes/ushipnetwork/blog.php
+++ wp-content/themes/ushipnetwork/blog.php
@@ -41,14 +41,34 @@ get_header(); ?>
 
     echo '</div>';
 
-    echo '<div class="posts">';
+    echo '<div class="posts-group">';
 
-    echo '<div class="posts-container catall">';
+    echo '<div class="posts-group-container catall">';
     query_posts("posts_per_page=12");
 
     if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-      <a href="<?php the_permalink();?>"><?php the_title(); ?></a>
+    <div class="post">
+      <?php if (has_post_thumbnail( $post->ID ) ): ?>
+        <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+        <div class="post-featureImage" style="background-image: url('<?php echo $image[0]; ?>')"></div>
+      <?php endif; ?>
+      <div>
+        <?php
+        $categories = get_the_category();
+        $separator = ', ';
+        $output = '';
+        if ( ! empty( $categories ) ) {
+            foreach( $categories as $category ) {
+                $output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ) . '">' . esc_html( $category->name ) . '</a>' . $separator;
+            }
+            echo trim( $output, $separator );
+        }
+        ?>
+      </div>
+      <h4 class="post-title"><?php the_title(); ?></h4>
+      <a class="emphasisLink" target="_blank" href="<?php the_permalink();?>">Read Article</a>
+    </div>
 
     <?php endwhile; endif;
 
@@ -61,7 +81,27 @@ get_header(); ?>
 
       if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-        <a href="<?php the_permalink();?>"><?php the_title(); ?></a>
+      <div>
+        <?php if (has_post_thumbnail( $post->ID ) ): ?>
+          <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+          <div class="post-featureImage" style="background-image: url('<?php echo $image[0]; ?>')"></div>
+        <?php endif; ?>
+        <div>
+          <?php
+          $categories = get_the_category();
+          $separator = ', ';
+          $output = '';
+          if ( ! empty( $categories ) ) {
+              foreach( $categories as $category ) {
+                  $output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ) . '">' . esc_html( $category->name ) . '</a>' . $separator;
+              }
+              echo trim( $output, $separator );
+          }
+          ?>
+        </div>
+        <h4 class="post-title"><?php the_title(); ?></h4>
+        <a class="emphasisLink" target="_blank" href="<?php the_permalink();?>">Read Article</a>
+      </div>
 
       <?php endwhile; endif; echo '</div>';?>
 
