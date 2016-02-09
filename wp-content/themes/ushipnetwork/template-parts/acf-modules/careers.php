aa0b88670679f185db7099531d09e047a1afd943
unknown <rmikeska@rudym.local.uship.com>
2016-02-09 15:21:26 -0600

    add new acf update

---
+++ wp-content/themes/ushipnetwork/template-parts/acf-modules/careers.php
@@ -0,0 +1,13 @@
+<section class="careersListings">
+
+  <script type="text/javascript">
+    jQuery(document).ready(function ($) {
+      $('style').remove();
+
+      $('.resumator-job-description').hide();
+    })
+  </script>
+
+  <script type="text/javascript" src="//app.jazz.co/widgets/basic/create/uShip" charset="utf-8"></script>
+
+</section>
