90624b00b449f44802e44b7032680dd1fc536382
unknown <rmikeska@rudym.local.uship.com>
2016-02-16 09:56:12 -0600

    try again to fix celular issue with loading jquery

--- wp-content/themes/ushipnetwork/functions.php
+++ wp-content/themes/ushipnetwork/functions.php
@@ -134,9 +134,6 @@ function ushipnetwork_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-
-	if ( is_page('Listings') )
-	  wp_enqueue_script( 'jazz-widget-fixer', get_template_directory_uri() . '/js/jazz-widget-fixer.js', array(), '20160216', true );
 }
 add_action( 'wp_enqueue_scripts', 'ushipnetwork_scripts' );
 
--- wp-content/themes/ushipnetwork/template-parts/acf-modules/careers.php
+++ wp-content/themes/ushipnetwork/template-parts/acf-modules/careers.php
@@ -1,5 +1,11 @@
 <section class="careersListings">
 
-  <script type="text/javascript" src="//app.jazz.co/widgets/basic/create/uShip" charset="utf-8"></script>
+  <script type="text/javascript">
+  // <![CDATA[
+    function loadJS(file){document.write("<" + "script type='text/javascript' src='" + file + "'></" + "script>")}
+    loadJS("/wp-content/themes/ushipnetwork/js/jazz-widget-fixer.js");
+    loadJS("//app.jazz.co/widgets/basic/create/uShip");
+  // ]]>
+  </script>
 
 </section>
