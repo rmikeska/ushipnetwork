2e0cf874507dcb362482280709533f1036ab2e54
unknown <rmikeska@rudym.local.uship.com>
2016-03-04 11:33:56 -0600

    clean up vertical centering for hero module

--- wp-content/themes/ushipnetwork/template-parts/acf-modules/hero.php
+++ wp-content/themes/ushipnetwork/template-parts/acf-modules/hero.php
@@ -3,13 +3,15 @@
 
 <section class="hero <?php echo $hero_size; ?>" style="background-image: url('<?php echo $hero_image['url']; ?>');">
   <div class="hero-container">
-    <h1 class="hero-headline"><?php the_sub_field('hero_headline'); ?></h1>
-    <?php if(get_sub_field('hero_subheadline')): ?>
-      <h5 class="hero-subheadline"><?php the_sub_field('hero_subheadline'); ?></h5>
-    <?php endif; ?>
-    <?php while(have_rows('hero_cta')): the_row(); ?>
-      <?php $hero_cta_url = get_sub_field('hero_cta_url'); ?>
-      <a class="hero-cta buttonGreen buttonLarge" href="<?php echo $hero_cta_url; ?>"><?php the_sub_field('hero_cta_text'); ?></a>
-    <?php endwhile; ?>
+    <div class="hero-content">
+      <h1 class="hero-headline"><?php the_sub_field('hero_headline'); ?></h1>
+      <?php if(get_sub_field('hero_subheadline')): ?>
+        <h5 class="hero-subheadline"><?php the_sub_field('hero_subheadline'); ?></h5>
+      <?php endif; ?>
+      <?php while(have_rows('hero_cta')): the_row(); ?>
+        <?php $hero_cta_url = get_sub_field('hero_cta_url'); ?>
+        <a class="hero-cta buttonGreen buttonLarge" href="<?php echo $hero_cta_url; ?>"><?php the_sub_field('hero_cta_text'); ?></a>
+      <?php endwhile; ?>
+    </div>
   </div>
 </section>
