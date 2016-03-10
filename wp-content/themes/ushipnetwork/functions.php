af5730655bdc7bad417fca3cbcceb8c93babc332
unknown <rmikeska@rudym.local.uship.com>
2016-03-10 09:58:32 -0600

    comment out Posts menu hiding on about. for now

--- wp-content/themes/ushipnetwork/functions.php
+++ wp-content/themes/ushipnetwork/functions.php
@@ -190,10 +190,10 @@ add_action('admin_init', 'remove_editor');
 /**
  * Remove Posts Menu on About Staging Site
  */
-function remove_about_posts_menu() {
-	$siteurl = site_url();
-	if ($siteurl == "http://ushipabout2.staging.wpengine.com") {
-		remove_menu_page('edit.php');
-	}
-}
-add_action( 'admin_menu', 'remove_about_posts_menu' );
+// function remove_about_posts_menu() {
+// 	$siteurl = site_url();
+// 	if ($siteurl == "http://ushipabout2.staging.wpengine.com") {
+// 		remove_menu_page('edit.php');
+// 	}
+// }
+// add_action( 'admin_menu', 'remove_about_posts_menu' );
