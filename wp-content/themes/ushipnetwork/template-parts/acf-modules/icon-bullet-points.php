1a99a1fbed599d52f9829e669c36527de7596628
unknown <rmikeska@rudym.local.uship.com>
2016-03-29 14:23:55 -0500

    fix up icon bullet point and slanted map styles

--- wp-content/themes/ushipnetwork/template-parts/acf-modules/icon-bullet-points.php
+++ wp-content/themes/ushipnetwork/template-parts/acf-modules/icon-bullet-points.php
@@ -1,14 +1,12 @@
 <section class="iconBulletPoints">
   <div class="iconBulletPoints-container">
     <?php while(have_rows('icon_bullet_points_row')): the_row(); ?>
+      <h5 class="iconBulletPoints-title"><?php the_sub_field('icon_bullet_points_row_title'); ?></h5>
       <div class="iconBulletPoints-row">
         <div class="iconBulletPoints-icon">
           <?php include("icons.php");?>
         </div>
-        <div class="iconBulletPoints-text">
-          <h5 class="iconBulletPoints-text-title"><?php the_sub_field('icon_bullet_points_row_title'); ?></h5>
-          <div class="iconBulletPoints-text-body"><?php the_sub_field('icon_bullet_points_row_text'); ?></div>
-        </div>
+        <div class="iconBulletPoints-body"><?php the_sub_field('icon_bullet_points_row_text'); ?></div>
       </div>
     <?php endwhile; ?>
   </div>
