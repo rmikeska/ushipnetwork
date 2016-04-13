13c62afbc6be678d57f456b8172db77815f7420c
unknown <rmikeska@rudym.local.uship.com>
2016-04-13 13:04:30 -0500

    remove neat, navigation js, and update more styles

--- wp-content/themes/ushipnetwork/functions.php
+++ wp-content/themes/ushipnetwork/functions.php
@@ -124,8 +124,6 @@ function ushipnetwork_scripts() {
 
 	wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/lib/jquery-2.2.0.min.js', array(), '20160121', true );
 
-	wp_enqueue_script( 'ushipnetwork-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
 	wp_enqueue_script( 'ushipnetwork-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	wp_enqueue_script( 'ushipnetwork-utilities', get_template_directory_uri() . '/js/utilities.js', array(), '20160218', true );
