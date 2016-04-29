fb02460a88ea8730096e769002b29065eb0a5eda
unknown <rmikeska@rudym.local.uship.com>
2016-04-29 18:14:54 -0500

    more lineheight shit

--- wp-content/themes/ushipnetwork/template-parts/acf-modules/split-screen.php
+++ wp-content/themes/ushipnetwork/template-parts/acf-modules/split-screen.php
@@ -5,7 +5,8 @@
       <?php $panel_url = get_sub_field('split_screen_panel_url'); ?>
       <a href="<?php echo $panel_url; ?>" class="splitScreen-panel" style="background-image: url('<?php echo $panel_image['url']; ?>');">
         <div class="splitScreen-panel-container">
-          <h1 class="splitScreen-panel-headline"><?php the_sub_field('split_screen_panel_headline'); ?><?php include( get_template_directory() . '/images/circled_arrow.svg'); ?></h1>
+          <h1 class="splitScreen-panel-headline"><?php the_sub_field('split_screen_panel_headline'); ?></h1>
+          <?php include( get_template_directory() . '/images/circled_arrow.svg'); ?>
           <h3 class="splitScreen-panel-subheadline"><?php the_sub_field('split_screen_panel_subheadline'); ?></h3>
         </div>
       </a>
@@ -17,7 +18,8 @@
       <a href="<?php echo $strip_url; ?>" class="splitScreen-strip">
         <div class="splitScreen-strip-container">
           <div class="splitScreen-strip-headline-container">
-            <h2 class="splitScreen-strip-headline"><?php the_sub_field('split_screen_strip_headline'); ?><?php include( get_template_directory() . '/images/circled_arrow.svg'); ?></h2>
+            <h2 class="splitScreen-strip-headline"><?php the_sub_field('split_screen_strip_headline'); ?></h2>
+            <?php include( get_template_directory() . '/images/circled_arrow.svg'); ?>
           </div>
           <h4 class="splitScreen-strip-subheadline"><?php the_sub_field('split_screen_strip_subheadline'); ?></h4>
         </div>
