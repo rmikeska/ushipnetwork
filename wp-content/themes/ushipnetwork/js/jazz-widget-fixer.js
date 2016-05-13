5166abec6b136ab73e4a2dd1809b883f17eaa5f9
unknown <rmikeska@rudym.local.uship.com>
2016-05-13 14:44:17 -0500

    add proper button styles to jazz widget

--- wp-content/themes/ushipnetwork/js/jazz-widget-fixer.js
+++ wp-content/themes/ushipnetwork/js/jazz-widget-fixer.js
@@ -2,6 +2,10 @@ jQuery(document).ready(function ($) {
 
     $('style, .resumator-job-view-details, .resumator-job-link').remove();
 
+    $('input.resumator-apply-button').addClass('button buttonPrimary buttonSmall');
+
+    $('input.resumator-forward-button').addClass('button buttonSecondary buttonSmall');
+
     $('.resumator-job').each (function () {
         $(this).prepend('<a class="resumator-view-details">View Details</a>');
     });
