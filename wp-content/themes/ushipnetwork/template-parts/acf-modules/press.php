2009e5f78d8fcb2da7e0465b8f0a5487a862659b
unknown <rmikeska@rudym.local.uship.com>
2016-06-01 13:27:46 -0500

    forgot the section dividers for mobile on the press stril

--- wp-content/themes/ushipnetwork/template-parts/acf-modules/press.php
+++ wp-content/themes/ushipnetwork/template-parts/acf-modules/press.php
@@ -2,9 +2,11 @@
   <?php query_posts('category_name=press&posts_per_page=3'); ?>
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <div class="pressStrip-block">
-      <div class="post-caption">Latest&nbsp;&nbsp;|&nbsp;&nbsp;<span class="post-time"><?php the_time('M j, Y'); ?></span></div>
-      <a class="post-title" href="<?php the_permalink();?>"><?php the_title(); ?></a>
-      <a class="tertiaryLink tertiaryLinkDark" role="button" href="<?php the_permalink();?>">Read Press Release<?php include( get_template_directory() . '/images/arrow_link.svg'); ?></a>
+      <div class="pressStrip-block-inner">
+        <div class="post-caption">Latest&nbsp;&nbsp;|&nbsp;&nbsp;<span class="post-time"><?php the_time('M j, Y'); ?></span></div>
+        <a class="post-title" href="<?php the_permalink();?>"><?php the_title(); ?></a>
+        <a class="tertiaryLink tertiaryLinkDark" role="button" href="<?php the_permalink();?>">Read Press Release<?php include( get_template_directory() . '/images/arrow_link.svg'); ?></a>
+      </div>
     </div>
   <?php endwhile; endif; ?>
   <?php wp_reset_query(); ?>
