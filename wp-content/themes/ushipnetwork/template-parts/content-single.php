68a1725cf8d95a12f302d52cf6a5266ba4ef4587
unknown <rmikeska@rudym.local.uship.com>
2016-06-15 12:55:49 -0500

    put comments in the right place this time

--- wp-content/themes/ushipnetwork/template-parts/content-single.php
+++ wp-content/themes/ushipnetwork/template-parts/content-single.php
@@ -41,6 +41,14 @@
 	<div class="entry-footer">
 		<?php ushipnetwork_entry_footer(); ?>
 	</div>
+
+	<?php
+	  // If comments are open or we have at least one comment, load up the comment template.
+	  if ( comments_open() || get_comments_number() ) :
+	    comments_template();
+	  endif;
+	?>
+
 </article>
 
 <section class="relatedPosts">
