773b7e06ece9a932a911110d1edcc425fc69ed5c
unknown <rmikeska@rudym.local.uship.com>
2016-06-23 09:32:41 -0500

    try this new video embed option

--- wp-content/themes/ushipnetwork/template-parts/acf-modules/video.php
+++ wp-content/themes/ushipnetwork/template-parts/acf-modules/video.php
@@ -5,13 +5,51 @@
     <?php $video_poster = get_sub_field('video_poster'); ?>
     <?php $video_url = get_sub_field('youtube_url'); ?>
     <div class="videoFeature-video">
-      <video
-        class="video-js"
-        controls
-        preload="auto"
-        poster="<?php echo $video_poster['url']; ?>"
-        data-setup='{ "techOrder": ["youtube"], "sources": [{ "type": "video/youtube", "src": "<?php echo $video_url; ?>"}] }'>
-      </video>
+    <script type="text/javascript">
+
+    jQuery(document).ready(function ($) {
+      var tag = document.createElement('script');
+      tag.src = "//www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      var player;
+
+      onYouTubeIframeAPIReady = function () {
+          player = new YT.Player('player', {
+              height: '244',
+              width: '434',
+              videoId: "<?php echo $video_url; ?>",
+              playerVars: {
+                  'autoplay': 0,
+                  'rel': 0,
+                  'showinfo': 0
+              },
+              events: {
+                  'onStateChange': onPlayerStateChange
+              }
+          });
+      }
+
+      var p = document.getElementById ("player");
+      $(p).hide();
+
+      var t = document.getElementById ("thumbnail");
+      t.src = "<?php echo $video_poster['url']; ?>";
+
+      onPlayerStateChange = function (event) {
+          if (event.data == YT.PlayerState.ENDED) {
+              $('.start-video').fadeIn('normal');
+          }
+      }
+    })
+    </script>
+
+    <div id="player"></div>
+    <div id="thumbnail_container" class="thumbnail_container">
+        <img class="thumbnail" id="thumbnail" />
+    </div>
+    <a class="start-video"><img width="64" src="https://ushiplearn.staging.wpengine.com/wp-content/themes/ushipnetwork/images/play_button.svg"></a>
     </div>
     <?php while(have_rows('video_cta')): the_row(); ?>
       <?php $video_cta_url = get_sub_field('video_cta_url'); ?>
