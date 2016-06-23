81105616017595c29c5dad6b5c58bd51ee674c51
unknown <rmikeska@rudym.local.uship.com>
2016-06-23 13:30:45 -0500

    remove yt api script until i can figure out how to make it work for any number of videos

--- wp-content/themes/ushipnetwork/template-parts/acf-modules/video.php
+++ wp-content/themes/ushipnetwork/template-parts/acf-modules/video.php
@@ -4,59 +4,9 @@
     <h6 class="videoFeature-subintro"><?php the_sub_field('video_intro_sub'); ?></h6>
     <div class="videoFeature-video">
       <div class="videoFeature-video-inner">
-
         <?php $video_poster = get_sub_field('video_poster'); ?>
         <?php $video_id = get_sub_field('youtube_id'); ?>
-        <script type="text/javascript">
-
-        jQuery(document).ready(function ($) {
-            var tag = document.createElement('script');
-            tag.src = "//www.youtube.com/iframe_api";
-            var firstScriptTag = document.getElementsByTagName('script')[0];
-            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-            var player;
-
-            onYouTubeIframeAPIReady = function () {
-                player = new YT.Player('player', {
-                    videoId: "<?php echo $video_id; ?>",
-                    playerVars: {
-                        'autoplay': 0,
-                        'rel': 0,
-                        'showinfo': 0
-                    },
-                    events: {
-                        'onStateChange': onPlayerStateChange
-                    }
-                });
-            }
-
-            var p = document.getElementById ("player");
-            $(p).hide();
-
-            var t = document.getElementById ("videoFeature-video-poster-image");
-            t.src = "<?php echo $video_poster['url']; ?>";
-
-            onPlayerStateChange = function (event) {
-                if (event.data == YT.PlayerState.ENDED) {
-                    $('.videoFeature-video-playButton').fadeIn('normal');
-                }
-            }
-
-            $(document).on('click', '.videoFeature-video-playButton', function () {
-                $(this).hide();
-                $("#player").show();
-                $("#videoFeature-video-poster").hide();
-                player.playVideo();
-            });
-        })
-        </script>
-
-        <div id="player"></div>
-        <div id="videoFeature-video-poster" class="videoFeature-video-poster">
-            <img class="videoFeature-video-poster-image" id="videoFeature-video-poster-image" />
-        </div>
-        <a class="videoFeature-video-playButton"><?php include( get_template_directory() . '/images/play_button.svg'); ?></a>
+        <iframe id="player" src="//www.youtube.com/embed/<?php echo $video_id; ?>?controls=0&amp;showinfo=0" frameborder="0" allowfullscreen=""></iframe>
       </div>
     </div>
 
