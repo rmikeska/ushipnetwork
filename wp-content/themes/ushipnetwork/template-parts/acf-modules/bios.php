780850de8ed19a37d3476d828b1cbf45ebab1658
unknown <rmikeska@rudym.local.uship.com>
2016-07-01 13:53:07 -0500

    sometimes i think flexbox might not be worth it. so many more style rules needed unless you just want full centering

--- wp-content/themes/ushipnetwork/template-parts/acf-modules/bios.php
+++ wp-content/themes/ushipnetwork/template-parts/acf-modules/bios.php
@@ -7,15 +7,17 @@
       <h6 class="bios-subintro"><?php the_sub_field('bios_intro_sub'); ?></h6>
     <?php endif; ?>
     <div class="bios-group">
-    <?php while(have_rows('bios_content')): the_row(); ?>
-      <?php $bios_image = get_sub_field('bios_image'); ?>
-      <?php $bios_link_url = get_sub_field('bios_link_url'); ?>
-      <a class="bios-block" target="_blank" href="<?php echo $bios_link_url; ?>">
-        <img class="bios-image" src="<?php echo $bios_image['url']; ?>">
-        <p class="bios-name"><?php the_sub_field('bios_name'); ?></p>
-        <div class="bios-description"><?php the_sub_field('bios_description'); ?></div>
-      </a>
-    <?php endwhile; ?>
+      <div class="bios-group-container">
+        <?php while(have_rows('bios_content')): the_row(); ?>
+          <?php $bios_image = get_sub_field('bios_image'); ?>
+          <?php $bios_link_url = get_sub_field('bios_link_url'); ?>
+          <a class="bios-block" target="_blank" href="<?php echo $bios_link_url; ?>">
+            <img class="bios-image" src="<?php echo $bios_image['url']; ?>">
+            <p class="bios-name"><?php the_sub_field('bios_name'); ?></p>
+            <div class="bios-description"><?php the_sub_field('bios_description'); ?></div>
+          </a>
+        <?php endwhile; ?>
+      </div>
     </div>
   </div>
 </section>
