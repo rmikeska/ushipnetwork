f91c2ef2881eaf5d8b40b8321cdac61e4e0ada79
unknown <rmikeska@rudym.local.uship.com>
2016-07-05 11:10:01 -0500

    rework the press page to include a media coverage post feed

--- wp-content/themes/ushipnetwork/template-parts/acf-modules/press.php
+++ wp-content/themes/ushipnetwork/template-parts/acf-modules/press.php
@@ -1,13 +1,45 @@
 <section class="pressStrip">
-  <?php query_posts('category_name=press&posts_per_page=3'); ?>
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <div class="pressStrip-block">
-      <div class="pressStrip-block-inner">
-        <div class="post-caption">Latest&nbsp;&nbsp;|&nbsp;&nbsp;<span class="post-time"><?php the_time('M j, Y'); ?></span></div>
-        <a class="post-title" href="<?php the_permalink();?>"><?php the_title(); ?></a>
-        <a class="tertiaryLink tertiaryLinkDark" role="button" href="<?php the_permalink();?>">Read Press Release<?php include( get_template_directory() . '/images/arrow_link.svg'); ?></a>
-      </div>
+  <div class="pressStrip-container">
+    <div class="pressStrip-column">
+      <h3 class="pressStrip-column-title">Press Releases</h3>
+      <?php query_posts('category_name=press-release&posts_per_page=3'); ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="pressStrip-block">
+          <div class="pressStrip-block-inner">
+            <div class="post-caption">Latest&nbsp;&nbsp;|&nbsp;&nbsp;<span class="post-time"><?php the_time('M j, Y'); ?></span></div>
+            <a class="post-title" href="<?php the_permalink();?>"><?php the_title(); ?></a>
+          </div>
+        </div>
+      <?php endwhile; endif; ?>
+      <?php wp_reset_query(); ?>
+      <a class="tertiaryLink tertiaryLinkDark" role="button" href="/press-release/">See All Press Releases<?php include( get_template_directory() . '/images/arrow_link.svg'); ?></a>
     </div>
-  <?php endwhile; endif; ?>
-  <?php wp_reset_query(); ?>
+    <div class="pressStrip-column">
+      <h3 class="pressStrip-column-title">Media Coverage</h3>
+      <?php query_posts('category_name=media-coverage&posts_per_page=3'); ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="pressStrip-block">
+          <div class="pressStrip-block-inner">
+          <div class="post-caption">
+            <?php
+            $posttags = get_the_tags();
+            $count=0;
+            if ($posttags) {
+              foreach($posttags as $tag) {
+                $count++;
+                if (1 == $count) {
+                  echo $tag->name . '&nbsp;&nbsp;|&nbsp;&nbsp;';
+                }
+              }
+            }
+            ?>
+            <span class="post-time"><?php the_time('M j, Y'); ?></span></div>
+            <a class="post-title" href="<?php the_permalink();?>"><?php the_title(); ?></a>
+          </div>
+        </div>
+      <?php endwhile; endif; ?>
+      <?php wp_reset_query(); ?>
+      <a class="tertiaryLink tertiaryLinkDark" role="button" href="/media-coverage/">See All Media Coverage<?php include( get_template_directory() . '/images/arrow_link.svg'); ?></a>
+    </div>
+  </div>
 </section>
