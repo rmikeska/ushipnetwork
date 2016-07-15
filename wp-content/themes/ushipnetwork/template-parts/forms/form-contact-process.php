b45e56ed257b614c279cd224ac226fbfcee139c4
unknown <rmikeska@rudym.local.uship.com>
2016-07-15 16:25:13 -0500

    clean up contact us form email output

--- wp-content/themes/ushipnetwork/template-parts/forms/form-contact-process.php
+++ wp-content/themes/ushipnetwork/template-parts/forms/form-contact-process.php
@@ -14,11 +14,11 @@
     $subject = $inquiry . $_REQUEST['subject'];
 
     $fields = array();
-    $fields{"subject"} = "Subject";
-    $fields{"email"} = "Email";
+    // $fields{"subject"} = "Subject";
+    // $fields{"email"} = "Email";
     $fields{"question"} = "Question/Problem/Concern";
 
-    $body = "Sent via the contact form on https://learn.uship.com/contact/:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+    $body = "Sent via the contact form on https://learn.uship.com/contact/\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
 
     $send = mail($to, $subject, $body, $headers);
 
