2563c08956af904a08c19fddefd24aad1233a336
unknown <rmikeska@rudym.local.uship.com>
2016-08-01 15:04:18 -0500

    add favicon set. rework subnav styles a bit

--- wp-content/themes/ushipnetwork/header.php
+++ wp-content/themes/ushipnetwork/header.php
@@ -17,6 +17,8 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<?php include("template-parts/favicons.php");?>
+
 <?php wp_head(); ?>
 </head>
 
