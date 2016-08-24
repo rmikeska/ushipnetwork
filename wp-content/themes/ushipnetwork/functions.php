0aada25d96416cdb40b3134a64199daa167888a6
unknown <rmikeska@rudym.local.uship.com>
2016-08-24 17:21:17 -0500

    do another test push or nothing to see if htaccess on staging still works fine

--- wp-content/themes/ushipnetwork/functions.php
+++ wp-content/themes/ushipnetwork/functions.php
@@ -277,7 +277,7 @@ if( function_exists('acf_add_options_page') ) {
   acf_add_options_page(array(
     'page_title'  => 'Localization',
     'menu_title'  => 'Localization',
-    'menu_slug'   => 'localization-settings'
+    'menu_slug'   => 'localization'
   ));
 
 }
