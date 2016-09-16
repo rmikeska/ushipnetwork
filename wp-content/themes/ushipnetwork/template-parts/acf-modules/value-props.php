c2363c2837b43ffdc8011b5a865add76b8b4776c
unknown <rmikeska@rudym.local.uship.com>
2016-09-16 15:21:43 -0500

    forgot to add files

---
+++ wp-content/themes/ushipnetwork/template-parts/acf-modules/value-props.php
@@ -0,0 +1,32 @@
+<section class="valueProps">
+  <div class="valueProps-container">
+  <div class="valueProps-item">
+      <svg class="valueProps-item-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 38"><path d="M36,5.9a2.58,2.58,0,0,0-3-2.5A47,47,0,0,1,26.11,4,9,9,0,0,1,19.83.81a2.52,2.52,0,0,0-3.66,0A9,9,0,0,1,9.89,4,47,47,0,0,1,3,3.4,2.58,2.58,0,0,0,0,5.9c-0.13,4.71.12,15.51,3.57,21,3.68,5.81,10.17,8.82,13,10.69a2.52,2.52,0,0,0,2.78,0c2.84-1.87,9.33-4.88,13-10.69C35.85,21.41,36.1,10.61,36,5.9ZM16.3,25.19l-5.94-5.77,2.14-2.12,3.43,3.34,7.34-7.83,2.38,2.12Z" fill="#231f20" fill-rule="evenodd"/></svg>
+      <div class="valueProps-item-text">
+          <h5>Smart Shipping</h5>
+          <h5><a href="https://learn.uship.com/shippers/#smart-shipping">Learn More</a></h5>
+      </div>
+  </div>
+  <div class="valueProps-item">
+      <svg class="valueProps-item-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38 38"><path d="M19,0A19,19,0,1,0,38,19,19,19,0,0,0,19,0Zm6.336,21.9661a1.27,1.27,0,0,1-1.27,1.27h0a1.2694,1.2694,0,0,1-1.2693-1.27V16.9982l-7.9664,7.9664a1.27,1.27,0,0,1-1.7956-1.7953l7.9664-7.9664H16.0337a1.27,1.27,0,0,1,0-2.539h8.0329a1.2736,1.2736,0,0,1,1.27,1.2692v8.033Z" transform="translate(0 0)" fill="#231f20" fill-rule="evenodd"/></svg>
+      <div class="valueProps-item-text">
+          <h5>Easy Tracking</h5>
+          <h5><a href="https://learn.uship.com/shippers/#easy-tracking">Learn More</a></h5>
+      </div>
+  </div>
+  <div class="valueProps-item">
+      <svg class="valueProps-item-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 39 27"><path d="M0,25.16A1.83,1.83,0,0,0,1.83,27H37.17A1.83,1.83,0,0,0,39,25.16V11H0V25.16ZM3.66,17.8H21.94v1.84H3.66V17.8Zm0,3.68H11v1.84H3.66V21.48ZM37.17,0H1.83A1.84,1.84,0,0,0,0,1.84V7.36H39V1.84A1.84,1.84,0,0,0,37.17,0Z" transform="translate(0 0)" fill="#231f20"/></svg>
+      <div class="valueProps-item-text">
+          <h5>Secure Payments</h5>
+          <h5><a href="https://learn.uship.com/shippers/#secure-payments">Learn More</a></h5>
+      </div>
+  </div>
+  <div class="valueProps-item">
+      <svg class="valueProps-item-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38 38"><path d="M19,0A19,19,0,1,0,38,19,19,19,0,0,0,19,0ZM16.78,27.25L9,19.56l2.79-2.82,4.48,4.45,9.58-10.44L29,13.57Z" transform="translate(0)" fill="#231f20"/></svg>
+      <div class="valueProps-item-text">
+          <h5>uShip Guarantee</h5>
+          <h5><a href="https://learn.uship.com/shippers/#uship-guarantee">Learn More</a></h5>
+      </div>
+  </div>
+  </div>
+</section>
