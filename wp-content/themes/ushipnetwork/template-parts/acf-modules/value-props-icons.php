0f0c332ae22173526c72c5e59e41204e6988946d
unknown <rmikeska@rudym.local.uship.com>
2016-09-21 11:44:02 -0500

    clean up existing svg icons. rework value props module

---
+++ wp-content/themes/ushipnetwork/template-parts/acf-modules/value-props-icons.php
@@ -0,0 +1,17 @@
+<?php if(get_sub_field('value_props_icon') == "icon_circledcheck"): ?>
+    <?php include( get_template_directory() . '/images/icons/value-props/icon_CircledCheck.svg'); ?>
+<?php elseif (get_sub_field('value_props_icon') == "icon_circleddiagonalarrow"): ?>
+    <?php include( get_template_directory() . '/images/icons/value-props/icon_CircledDiagonalArrow.svg'); ?>
+<?php elseif (get_sub_field('value_props_icon') == "icon_creditcard"): ?>
+    <?php include( get_template_directory() . '/images/icons/value-props/icon_CreditCard.svg'); ?>
+<?php elseif (get_sub_field('value_props_icon') == "icon_shieldcheck"): ?>
+    <?php include( get_template_directory() . '/images/icons/value-props/icon_ShieldCheck.svg'); ?>
+<?php elseif (get_sub_field('value_props_icon') == "icon_desktop"): ?>
+    <?php include( get_template_directory() . '/images/icons/value-props/icon_Desktop.svg'); ?>
+<?php elseif (get_sub_field('value_props_icon') == "icon_frontfacingtruck"): ?>
+    <?php include( get_template_directory() . '/images/icons/value-props/icon_FrontFacingTruck.svg'); ?>
+<?php elseif (get_sub_field('value_props_icon') == "icon_newspaper"): ?>
+    <?php include( get_template_directory() . '/images/icons/value-props/icon_Newspaper.svg'); ?>
+<?php elseif (get_sub_field('value_props_icon') == "icon_rotatingarrows"): ?>
+    <?php include( get_template_directory() . '/images/icons/value-props/icon_RotatingArrows.svg'); ?>
+<?php endif; ?>
