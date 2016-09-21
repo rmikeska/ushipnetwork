2c9aa80f50ebed209e0b417b965847ac959f1202
unknown <rmikeska@rudym.local.uship.com>
2016-09-21 14:07:48 -0500

    reword some value prop styles. echo site URL where we want relative path links so that it works on the global sites

--- wp-content/themes/ushipnetwork/template-parts/acf-modules/split-screen.php
+++ wp-content/themes/ushipnetwork/template-parts/acf-modules/split-screen.php
@@ -3,7 +3,8 @@
     <?php while(have_rows('split_screen_panel')): the_row(); ?>
       <?php $panel_image = get_sub_field('split_screen_panel_image'); ?>
       <?php $panel_url = get_sub_field('split_screen_panel_url'); ?>
-      <a href="<?php echo $panel_url; ?>" class="splitScreen-panel" style="background-image: url('<?php echo $panel_image['url']; ?>');">
+      <?php $panel_url_full = get_site_url() . $panel_url; ?>
+      <a href="<?php echo $panel_url_full; ?>" class="splitScreen-panel" style="background-image: url('<?php echo $panel_image['url']; ?>');">
         <div class="splitScreen-panel-container">
           <h1 class="splitScreen-panel-headline"><?php the_sub_field('split_screen_panel_headline'); ?></h1>
           <?php include( get_template_directory() . '/images/circled_arrow.svg'); ?>
@@ -15,7 +16,8 @@
   <div class="splitScreen-row">
     <?php while(have_rows('split_screen_strip')): the_row(); ?>
       <?php $strip_url = get_sub_field('split_screen_strip_url'); ?>
-      <a href="<?php echo $strip_url; ?>" class="splitScreen-strip">
+      <?php $strip_url_full = get_site_url() . $strip_url; ?>
+      <a href="<?php echo $strip_url_full; ?>" class="splitScreen-strip">
         <div class="splitScreen-strip-container">
           <h2 class="splitScreen-strip-headline"><?php the_sub_field('split_screen_strip_headline'); ?></h2>
           <h4 class="splitScreen-strip-subheadline"><?php the_sub_field('split_screen_strip_subheadline'); ?></h4>
