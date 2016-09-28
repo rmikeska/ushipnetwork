808c6f9c002526af8b4c751568447686dd7571d3
unknown <rmikeska@rudym.local.uship.com>
2016-09-28 13:35:08 -0500

    try this

--- wp-content/themes/ushipnetwork/careers-listings.php
+++ wp-content/themes/ushipnetwork/careers-listings.php
@@ -10,7 +10,7 @@ get_header(); ?>
   <?php include("template-parts/acf-modules/top-content.php");?>
 
   <section>
-
+    [feedzy-rss feeds="http://newton.newtonsoftware.com/career/CareerAtomFeed.action?clientId=8a788267569c34d80156b3030a450425"]
   </section>
 
 <?php get_footer(); ?>
