d1da0fe86c4fa05bebb3fa2e45d6ac7910e0c9a7
unknown <rmikeska@rudym.local.uship.com>
2016-09-28 13:47:54 -0500

    fuck newtonsoftware

--- wp-content/themes/ushipnetwork/careers-listings.php
+++ wp-content/themes/ushipnetwork/careers-listings.php
@@ -10,7 +10,7 @@ get_header(); ?>
   <?php include("template-parts/acf-modules/top-content.php");?>
 
   <section>
-    [feedzy-rss feeds="http://newton.newtonsoftware.com/career/CareerAtomFeed.action?clientId=8a788267569c34d80156b3030a450425"]
+    <script id="gnewtonjs" type="text/javascript" src="//newton.newtonsoftware.com/career/iframe.action?clientId=8a788267569c34d80156b3030a450425"></script>
   </section>
 
 <?php get_footer(); ?>
