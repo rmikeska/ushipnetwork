125fb72bb763d13d4b43e1d4485f235a4e1a8eff
unknown <rmikeska@rudym.local.uship.com>
2016-10-21 17:26:01 -0500

    i don't know man

--- wp-content/themes/ushipnetwork/js/tabbed-page-v2.js
+++ wp-content/themes/ushipnetwork/js/tabbed-page-v2.js
@@ -1,6 +1,20 @@
 (function ($) {
 
 
+    setInterval(function () {
+        $('.introSlide-slideshow').each(function () {
+            if ($(this).find('.introSlide-slideshow-slide').length > 1) {
+                $(this).find('.introSlide-slideshow-slide:first')
+                .fadeOut(500)
+                .next()
+                .fadeIn(500)
+                .end()
+                .appendTo($(this));
+            }
+        });
+    },  3000);
+
+
 
     // Add classes to larger tab menus for specific wide breakpoints
     $('.tabbedPageMenu-list').each(function () {
