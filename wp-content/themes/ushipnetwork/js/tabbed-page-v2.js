441620ea8af19f75feb4ca3557ef40b09f5803dd
unknown <rmikeska@rudym.local.uship.com>
2016-10-21 10:55:13 -0500

    try this out pt 2

--- wp-content/themes/ushipnetwork/js/tabbed-page-v2.js
+++ wp-content/themes/ushipnetwork/js/tabbed-page-v2.js
@@ -89,4 +89,20 @@
         }
     });
 
+    // Intro slide module slideshow effects
+    $('.bodyContent .introSlide-slideshow > .introSlide-slideshow-slide:first-child').show();
+
+    setInterval(function () {
+        $('.bodyContent .introSlide .introSlide-container .introSlide-slideshow').each(function () {
+            if ($(this).find('div.introSlide-slideshow-slide').length > 1) {
+                $(this).find('div.introSlide-slideshow-slide:first')
+                .fadeOut(500)
+                .next()
+                .fadeIn(500)
+                .end()
+                .appendTo($(this));
+            }
+        });
+    },  3000);
+
 })(jQuery);
