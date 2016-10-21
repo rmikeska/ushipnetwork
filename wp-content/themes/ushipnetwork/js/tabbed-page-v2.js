188dd88ad262855846a8a2cf2037e1e022c6b87b
unknown <rmikeska@rudym.local.uship.com>
2016-10-21 10:59:15 -0500

    fuck it keep trying

--- wp-content/themes/ushipnetwork/js/tabbed-page-v2.js
+++ wp-content/themes/ushipnetwork/js/tabbed-page-v2.js
@@ -29,6 +29,22 @@
         $.get(firstPage).done(function (data) {
             $('.tabContent').html($(data).find('.bodyContent'));
         })
+
+        // Intro slide module slideshow effects
+        $('.introSlide-slideshow > .introSlide-slideshow-slide:first-child').show();
+
+        setInterval(function () {
+            $('.introSlide .introSlide-container .introSlide-slideshow').each(function () {
+                if ($(this).find('div.introSlide-slideshow-slide').length > 1) {
+                    $(this).find('div.introSlide-slideshow-slide:first')
+                    .fadeOut(500)
+                    .next()
+                    .fadeIn(500)
+                    .end()
+                    .appendTo($(this));
+                }
+            });
+        },  3000);
     }
 
 
@@ -56,6 +72,22 @@
 
         $('.tabbedPageMenu-list li').removeClass('tabbedPage-active');
         $('.tabbedPageMenu-list li a[href="' + url + '"]').parent().addClass('tabbedPage-active');
+
+        // Intro slide module slideshow effects
+        $('.introSlide-slideshow > .introSlide-slideshow-slide:first-child').show();
+
+        setInterval(function () {
+            $('.introSlide .introSlide-container .introSlide-slideshow').each(function () {
+                if ($(this).find('div.introSlide-slideshow-slide').length > 1) {
+                    $(this).find('div.introSlide-slideshow-slide:first')
+                    .fadeOut(500)
+                    .next()
+                    .fadeIn(500)
+                    .end()
+                    .appendTo($(this));
+                }
+            });
+        },  3000);
     };
 
 
@@ -88,21 +120,5 @@
             loadFirstPage();
         }
     });
-
-    // Intro slide module slideshow effects
-    $('.bodyContent .introSlide-slideshow > .introSlide-slideshow-slide:first-child').show();
-
-    setInterval(function () {
-        $('.bodyContent .introSlide .introSlide-container .introSlide-slideshow').each(function () {
-            if ($(this).find('div.introSlide-slideshow-slide').length > 1) {
-                $(this).find('div.introSlide-slideshow-slide:first')
-                .fadeOut(500)
-                .next()
-                .fadeIn(500)
-                .end()
-                .appendTo($(this));
-            }
-        });
-    },  3000);
 
 })(jQuery);
