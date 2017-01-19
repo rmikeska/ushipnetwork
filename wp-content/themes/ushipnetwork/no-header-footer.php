98fbe69b0d1f8a2b5587cfac87753f53e25524d4
unknown <rmikeska@rudym.local.uship.com>
2017-01-19 10:58:11 -0600

    update acfpro, add new page template that excludes header/footer

---
+++ wp-content/themes/ushipnetwork/no-header-footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package ushipnetwork
+ *
+ * template name: No Header and Footer
+ */
+?>
+
+<?php include("template-parts/acf-modules/top-content.php");?>
+<div class="bodyContent">
+<?php include("template-parts/acf-modules/body-content.php");?>
+</div>
+<?php include("template-parts/acf-modules/bottom-content.php");?>
