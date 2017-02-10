a10aaa97427fdfee1e7eb9155bb8525452f4bbba
unknown <rmikeska@rudym.local.uship.com>
2017-02-10 17:29:39 -0600

    add query string to all uship links acf option

--- wp-content/themes/ushipnetwork/template-parts/content-page.php
+++ wp-content/themes/ushipnetwork/template-parts/content-page.php
@@ -9,6 +9,23 @@
 
 ?>
 
+<?php if ($query_string = get_field('add_query_string')): ?>
+  <script type="text/javascript">
+    jQuery(document).ready(function ($) {
+      var acfQueryString = '<?php echo $query_string; ?>';
+
+      $("a[href*='uship.com']").each(function () {
+          var ushipHref = $(this).attr('href');
+
+          if (ushipHref) {
+              ushipHref += (ushipHref.match(/\?/) ? '&' : '?') + acfQueryString;
+              $(this).attr('href', ushipHref);
+          }
+      });
+    })
+  </script>
+<?php endif; ?>
+
 <?php include("acf-modules/top-content.php");?>
 <div class="bodyContent">
 <?php include("acf-modules/body-content.php");?>
